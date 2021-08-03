aa84ca2449bceb676b6f5ebf18eaeac463808660
Alessandro Bossi <alebossi96@hotmail.it>
2021-08-03 14:19:48 +0200

    modificato TDDRS

--- reconstructTDDR.py
+++ reconstructTDDR.py
@@ -6,7 +6,7 @@ from PIL import Image
 from scipy.sparse.linalg import lsmr
 from scipy.linalg import hadamard
 import librerieTesi.hadamardOrdering as hadamardOrdering
-
+from sklearn import linear_model
 
 def readSdt(fileName, numBanks):
 	data = []
@@ -29,7 +29,7 @@ def readSdt(fileName, numBanks):
 	return (time,data)
 
 class reconstructTDDR:
-	def __init__(self,fileName,nBanks,nBasis,nMeas, rastOrHad, lambda_0):
+	def __init__(self,fileName,nBanks,nBasis,nMeas, rastOrHad, lambda_0,method, lassoAlpha=0, compress = True):
 		self.nBanks =nBanks
 		self.nBasis = nBasis
 		self.nMeas = nMeas
@@ -40,9 +40,20 @@ class reconstructTDDR:
 		self.lambda_0 = lambda_0
 		self.calibrationToWl()
 		self.calibrateToWn()
+		self.compress = compress
+		self.method=method
+		if method == "lsmr":
+			print("lsmr")
+			self.reg = linear_model.LinearRegression()
+		elif method == "Lasso":
+			print("Lasso")
+			self.reg = linear_model.Lasso(alpha=lassoAlpha)
+		elif method == "Ridge":
+			print("Ridge")
+			self.reg = linear_model.Ridge(alpha=lassoAlpha)
+				
 		self.execute()
-		
-		
+
 	def calibrationToWl(self):
 		p = np.array([  1.51491966* 64/self.nBasis, 809.28844682])
 		self.wavelength=np.zeros(self.nBasis)
@@ -57,31 +68,48 @@ class reconstructTDDR:
 		if self.rastOrHad == "Had":
 			self.reconstructHadamard()
 			
+			
 	def accumulate(self):
-		self.tot = np.zeros((self.nBasis,self.nPoints ))
-		i = 0
-		for el in self.data:
-			idx = i%self.nBasis		
-			self.tot[idx,:]+= el
-			i+=1
-		return
-
+		H =  0.5*(hadamardOrdering.cake_cutting(self.nBasis) + np.ones((self.nBasis, self.nBasis)))
+		if self.compress:
+			self.tot = np.zeros((self.nBasis,self.nPoints ))
+			i = 0
+			for el in self.data:
+				idx = i%self.nBasis		
+				self.tot[idx,:]+= el
+				i+=1
+			self.matrix = H
+			return
+		sz = len(self.data)
+		self.tot = self.data
+		self.matrix = np.zeros((sz, self.nBasis))
+		for i in range(sz):
+			self.matrix[i,:]= H[i%self.nBasis,:]
+			
 	#va considerato che sono 1 e -1 le basi di Hadamard e io uso 1 e 0
 
 	def reconstructHadamard(self):
-		dataNp = np.zeros((self.nBasis,self.nPoints )) #non nMeas perchè voglio dim uguali per la ricostruzione. Impongo però basi non misurate a 0
+		sz = len(self.tot)
+		dataNp = np.zeros((sz,self.nPoints )) #non nMeas perchè voglio dim uguali per la ricostruzione. Impongo però basi non misurate a 0
 	 
 		i = 0
 		for el in self.tot:
 			dataNp[i,:] = el 
 			i+=1 
-		H = 0.5*(hadamardOrdering.cake_cutting(self.nBasis) + np.ones((self.nBasis, self.nBasis)))
+		
 		#data è 2D
 
-		self.recons = np.zeros((nBasis,self.nPoints ))   
+		#self.recons = np.zeros((nBasis,self.nPoints ))
+		"""
+		self.reg.fit(self.matrix,dataNp)   
+		self.recons = self.reg.coef_
+		print(self.recons.shape)
+		"""
 		for i in range(self.nPoints ):
-			self.recons[:,i] = lsmr(H,dataNp[:,i])[0] #spero ordine corretto
+			self.reg.fit(self.matrix,dataNp[:,i])
+			self.recons[:,i] = self.reg.coef_
 		
+		#"""
 	def wlAtTimeGate(self, tGate):
 		idx = np.where(self.time == tGate)
 		return self.recons[:,idx]
@@ -95,35 +123,49 @@ class reconstructTDDR:
 		return self.wn
 	def reconstruction(self):
 		if self.rmv:
-			return self.recons[1:,:]
+
+			return self.recons[:,1:]
 		return self.recons
 """
 mancano calibrazioni
 """
 if __name__=="__main__":
-	print("test")	
+	
 	nBanks =40
 	nBasis = 64
 	nMeas = 64
 	fileName = "0406/m9"
 	lambda_0=780
-	test = reconstructTDDR(fileName,nBanks,nBasis,nMeas, "Had", lambda_0)
+	test = reconstructTDDR(fileName,nBanks,nBasis,nMeas, "Had", lambda_0,"Ridge", 100, True)
+	test.removeFirstLine(True)
 	from latex import latex
 	lat = latex("test")
 	plot = []
-	
-	name = lat.plotData2D(test.time,test.wavenumber(),test.reconstruction(),"time","ns","wavenumber","$cm^{-1}$","reconstruct m9","m9" )
-	plot.append(name)
-	name = lat.plotData2D(test.time,test.wavenumber(),test.reconstruction(),"time","ns","wavenumber","$cm^{-1}$","reconstruct m9","m9" )
-	plot.append(name)
-	name = lat.plotData2D(test.time,test.wavenumber(),test.reconstruction(),"time","ns","wavenumber","$cm^{-1}$","reconstruct m9","m9" )
-	plot.append(name)
-	name = lat.plotData2D(test.time,test.wavenumber(),test.reconstruction(),"time","ns","wavenumber","$cm^{-1}$","reconstruct m9","m9" )
-	plot.append(name)
-	name = lat.plotData2D(test.time,test.wavenumber(),test.reconstruction(),"time","ns","wavenumber","$cm^{-1}$","reconstruct m9","m9" )
-	plot.append(name)
-	lat.newFigure(plot, "m9")
+	print(test.reconstruction().shape)
+	name = lat.plotData2D(test.wavenumber(), test.time,test.reconstruction(),"time","ns","wavenumber","$cm^{-1}$","reconstruct m9","m9RidgeoComp" )
+	test = reconstructTDDR(fileName,nBanks,nBasis,nMeas, "Had", lambda_0,"Lasso", 0.1, True)
+	test.removeFirstLine(True)
+	from latex import latex
+	lat = latex("test")
+	plot = []
+	print(test.reconstruction().shape)
+	name = lat.plotData2D(test.wavenumber(), test.time,test.reconstruction(),"time","ns","wavenumber","$cm^{-1}$","reconstruct m9","m9LassoComp" )
+	test = reconstructTDDR(fileName,nBanks,nBasis,nMeas, "Had", lambda_0,"lsmr", 0.02, True)
+	test.removeFirstLine(True)
+	from latex import latex
+	lat = latex("test")
+	plot = []
+	print(test.reconstruction().shape)
+	name = lat.plotData2D(test.wavenumber(), test.time,test.reconstruction(),"time","ns","wavenumber","$cm^{-1}$","reconstruct m9","m9LsmrComp" )
 
+	"""
+	test = reconstructTDDR(fileName,nBanks,nBasis,nMeas, "Had", lambda_0,"lsmr", 0, False)
+	test.removeFirstLine(True)
+	from latex import latex
+	lat = latex("test")
+	plot = []
+	name = lat.plotData2D(test.time,test.wavenumber(),test.reconstruction(),"time","ns","wavenumber","$cm^{-1}$","reconstruct m9","m9Comp" )
+	"""
 
 """
 devo ancora fare il plot nel caso in cui voglio stampare solo time gate oppure solo linee
--- wlTT.py
+++ wlTT.py
@@ -28,36 +28,50 @@ class wavelengthTroughTime:
 		self.fileRay = fileRayleigh
 		self.timeRay= self.timeRayleigh()[1]
 		self.wl_0 = wl_0
-		
+		self.calibration()
 
 
 
 
-	def timeToWl(self,time): #from time to wavelength
+	def timeToWl700(self,time): #from time to wavelength
 		p = np.array([-1.4616e+26,    3.5911e+18,     -5.0642e+10 ,    9.6792e+02 ])
 		return p[0]*time**3+p[1]*time**2+p[2]*time**1+p[3]
-	
-
+	def timeToWl900(self,time): #from time to wavelength
+		p = np.array([ -3.3068e+27 ,  1.2054e+20,  -1.5128e+12,   7.4733e+03])
+		return p[0]*time**3+p[1]*time**2+p[2]*time**1+p[3]
+		
+	def timeToWl(self,time):
+		if(self.wl_0>900):
+			return self.timeToWl900(time)	
+		return self.timeToWl700(time)
 	def calibration(self):
-		lmd = np.zeros(len(self.data))
-		wavenumber = np.zeros(len(self.data))
+		self.lmd = np.zeros(len(self.data))
+		self.wavenumber = np.zeros(len(self.data))
 		f = (lambda time : self.timeToWl(time) -self.wl_0)
 		t_Ral = fsolve(f, x0 = self.wl_0 )[0]
 		offset= t_Ral-self.timeRay
 		for i in range(len(self.data)):
 			dt =  self.time[i]+offset; # s
-			lmd[i] = self.timeToWl(dt)
-			wavenumber[i] = (1/self.wl_0-1/lmd[i])*1e7
-		return(lmd, wavenumber)
+			self.lmd[i] = self.timeToWl(dt)
+			self.wavenumber[i] = (1/self.wl_0-1/self.lmd[i])*1e7
+		
 
 	def fwhmResolution(self):
+	#TODO scrivere meglio
 		data = readSdt(self.fileRay)[:,1]
 		pos0= self.timeRayleigh()[0]
-		fwhm_idx = peak_widths(data, pos0)[0]
+		
+		fwhm_idx = int(peak_widths(data, [pos0])[0])
 		(lmd,wn) = self.calibration()
+		fwhmTime = self.time[pos0-fwhm_idx]- self.time[pos0+fwhm_idx]
 		fwhmWL= lmd[pos0-fwhm_idx]-lmd[pos0+fwhm_idx]
 		fwhmWn = wn[pos0-fwhm_idx]-wn[pos0+fwhm_idx]
-		return(fwhmWL,fwhmWn)
+		return(fwhmTime,fwhmWL,fwhmWn)
+	def removeRayleigh(self, cm):
+		for i in range(len(self.wavenumber)):
+			if self.wavenumber[i]<cm:
+				return i
+		return len(self.wavenumber)
 		
 if __name__ == "__main__":
 	meas = wavelengthTroughTime("2106/m4.sdt","2106/rayleigh.sdt",1000)
